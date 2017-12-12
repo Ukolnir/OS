c941f4c00ffd3abf21a0992702850722b977e490
Ukolnir <metallwind@gmail.com>
2017-12-12 12:59:51 +0300

    unworking version

---
+++ POSIX9.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int fd1[2], fd2[2]; //Дескрипторы для каналов
+
+int main(){
+    pid_t pid;
+    pipe(fd1);
+    
+    
+    
+    
+    
+    
+}
